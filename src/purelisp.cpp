6995443247016f1de68bb1e71eb0fc474aacda69
yamacir-kit <httperror@404-notfound.jp>
2018-06-03 17:11:57 +0900

    Update tokenizer
    クラスとしての使用感はかなり良くなったはず

--- src/purelisp.cpp
+++ src/purelisp.cpp
@@ -41,32 +41,32 @@ namespace lisp
   class tokenizer
     : public std::vector<std::string>
   {
-  public:
-    tokenizer() = default;
-
-    template <typename... Ts>
-    tokenizer(Ts&&... args)
+    static decltype(auto) tokenize_(const std::string& s)
     {
-      (*this).operator()(std::forward<Ts>(args)...);
-    }
-
-    auto& operator()(const std::string& s)
-    {
-      clear();
+      std::vector<std::string> buffer {};
 
       for (auto iter {find_token_begin(std::begin(s), std::end(s))}; iter != std::end(s); iter = find_token_begin(iter, std::end(s)))
       {
-        emplace_back(iter, is_round_brackets(*iter) ? iter + 1 : find_token_end(iter, std::end(s)));
-        iter += std::size(back());
+        buffer.emplace_back(iter, is_round_brackets(*iter) ? iter + 1 : find_token_end(iter, std::end(s)));
+        iter += std::size(buffer.back());
       }
 
-      return *this;
+      return buffer;
     }
 
-    template <typename T>
-    static constexpr bool is_round_brackets(T&& c)
+  public:
+    template <typename... Ts>
+    tokenizer(Ts&&... args)
+      : std::vector<std::string> {std::forward<Ts>(args)...}
+    {}
+
+    tokenizer(const std::string& s)
+      : std::vector<std::string> {std::move(tokenize_(s))}
+    {}
+
+    auto& operator()(const std::string& s)
     {
-      return c == '(' || c == ')';
+      return *this = std::move(tokenize_(s));
     }
 
     friend auto operator<<(std::ostream& os, tokenizer& tokens)
@@ -80,6 +80,12 @@ namespace lisp
     }
 
   protected:
+    template <typename CharType>
+    static constexpr bool is_round_brackets(CharType&& c)
+    {
+      return c == '(' || c == ')';
+    }
+
     template <typename InputIterator>
     static constexpr auto find_token_begin(InputIterator first, InputIterator last)
       -> InputIterator
